a698bd6ae8c7c824753b531e400bbb38f216c478
Louis Mihaly <louis.mihaly@hotmail.com>
2016-08-30 19:28:56 +1000

    Retrying menu application

--- BCC_Bridge/MenuActivity.cs
+++ BCC_Bridge/MenuActivity.cs
@@ -8,18 +8,27 @@ using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
+using SupportToolbar = Android.Support.V7.Widget.Toolbar;
 
 namespace BCC_Bridge
 {
-	[Activity(Label = "MenuActivity")]
-	public class MenuActivity : Activity
+	[Activity(Label = "MenuActivity", Icon = "@drawable/icon", Theme ="@style/MyTheme")]
+	public class MenuActivity : ActionBarActivity
+
 	{
+		private SupportToolbar mToolbar;
+
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
 
+			SetContentView(Resource.Layout.Menu);
+
+			mToolbar = FindViewById<SupportToolbar>(Resource.Id.toolbar);
+			SetSupportActionBar(mToolbar);
 			// Create your application here
 		}
 	}
