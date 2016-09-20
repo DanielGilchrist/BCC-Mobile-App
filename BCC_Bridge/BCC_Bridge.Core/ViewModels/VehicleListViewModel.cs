c5488944e1274887485d2b9b7097214e131f37a4
Matt Downs <mattdowns6@gmail.com>
2016-09-21 09:30:33 +1000

    Fixed a bug where the VehicleListView would crash on opening.

--- BCC_Bridge/BCC_Bridge.Core/ViewModels/VehicleListViewModel.cs
+++ BCC_Bridge/BCC_Bridge.Core/ViewModels/VehicleListViewModel.cs
@@ -7,18 +7,19 @@ namespace BCC_Bridge.Core.ViewModels
 	{
 		public override void Start()
 		{
-			var test2 = new Vehicle() { name = "test 1" };
-			// *************************************************************************************
-			// This list.Add() Causes an exception to be thrown
-			// *************************************************************************************
-			//this.vehicles.Add(test2);
+			// Initialise the vehicles list
+			this.vehicles = new List<Vehicle> { };
+
+			// Add a few dummy vehicles
+			var test2 = new Vehicle() { name = "test", height = 200 };
+			this.vehicles.Add(test2);
+			this.vehicles.Add(test2);
+			this.vehicles.Add(test2);
+			this.vehicles.Add(test2);
+			this.vehicles.Add(test2);
 		}
 
 		private List<Vehicle> _vehicles;
-		// *************************************************************************************
-		// Initialising the list with an item in it also throws an exception.
-		// *************************************************************************************
-		//public List<Vehicle> vehicles = new List<Vehicle> { new Vehicle() { name = "test" } };
 		public List<Vehicle> vehicles
 		{
 			get { return _vehicles; }
