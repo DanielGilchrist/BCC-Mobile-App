8933b5c4bdc657a6e226154204a526b2c822d796
Daniel Gilchrist <danniieelg@gmail.com>
2016-10-05 11:22:18 +1000

    Added button for vehicle list

--- BCC_Bridge/BCC_Bridge.Core/ViewModels/FirstViewModel.cs
+++ BCC_Bridge/BCC_Bridge.Core/ViewModels/FirstViewModel.cs
@@ -5,6 +5,7 @@ namespace BCC_Bridge.Core.ViewModels
     public class FirstViewModel : MvxViewModel
     {
         public IMvxCommand MapsBtn { get { return ShowCommand<MapViewModel>(); } }
+        public IMvxCommand VehicleBtn { get { return ShowCommand<VehicleListViewModel>(); } }
 
         private MvxCommand ShowCommand<TViewModel>() where TViewModel : IMvxViewModel
         {
