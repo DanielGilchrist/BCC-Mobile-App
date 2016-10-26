4752a758c89665252fd17c0fc630a5122a776a96
Matt Downs <mattdowns6@gmail.com>
2016-10-26 10:47:12 +1000

    Added some methods to AddVehicleViewModel

--- BCC_Bridge/BCC_Bridge.Core/ViewModels/AddVehicleViewModel.cs
+++ BCC_Bridge/BCC_Bridge.Core/ViewModels/AddVehicleViewModel.cs
@@ -5,19 +5,40 @@ namespace BCC_Bridge.Core
 {
 	public class AddVehicleViewModel : MvxViewModel
 	{
-		public Vehicle Vehicle;
-
+		private Vehicle _vehicle;
 		private readonly IVehicleService _collectionService;
 
 		public AddVehicleViewModel(IVehicleService collectionService)
 		{
 			_collectionService = collectionService;
-			this.Vehicle = new Vehicle();
+			this._vehicle = new Vehicle();
 		}
 
-		public void saveVehicle()
+		public double VehicleHeight
 		{
-			_collectionService.Add(this.Vehicle);
+			get { return _vehicle.Height; }
+			set
+			{
+				_vehicle.Height = value;
+				RaisePropertyChanged(() => VehicleHeight);
+			}
+		}
+
+		public string VehicleName
+		{
+			get { return _vehicle.Name; }
+			set
+			{
+				_vehicle.Name = value;
+				RaisePropertyChanged(() => VehicleName);
+			}
+		}
+
+		public IMvxCommand SaveVehicleButton { get { return _saveVehicle(); } }
+
+		private MvxCommand _saveVehicle()
+		{
+			return new MvxCommand(() => _collectionService.Add(this._vehicle));
 		}
 	}
 }
